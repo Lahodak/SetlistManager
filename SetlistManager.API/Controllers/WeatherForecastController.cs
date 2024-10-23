78b9cb30e743db60718dd7bf1e9be90c93db9f8e
Adam Lahuta <lahutad@student.uzlabina.cz>
2024-10-24 00:10:14 +0200

    Dbshizz

--- SetlistManager.API/Controllers/WeatherForecastController.cs
+++ SetlistManager.API/Controllers/WeatherForecastController.cs
@@ -18,8 +18,8 @@ namespace SetlistManager.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("{id}")]
+        public IEnumerable<WeatherForecast> GetSong(int id)
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -28,6 +28,11 @@ namespace SetlistManager.API.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+        [HttpPost("")]
+        public IActionResult CreateToilet()
+        {
+            return Ok();
         }
     }
 }
