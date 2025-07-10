e80d62d72a4e6a8ba896b755c64ba6c835d39c92
lahutad <lahutad@student.uzlabina.cz>
2025-07-10 19:26:10 +0200

    Databáze funguje, testování se sample daty, přidání nullable u vazby User - Setlist

--- SetlistManager.API/Data/Entities/User.cs
+++ SetlistManager.API/Data/Entities/User.cs
@@ -7,7 +7,7 @@ public class User : Base
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-    public int RoomId { get; set; }
-    public Room Room { get; set; }
+    public int? RoomId { get; set; }
+    public Room? Room { get; set; }
     public List<Instrument> Instruments { get; set; }
 }
---
+++ SetlistManager.API/Migrations/20250710171413_AddedNullableForUserRoomId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SetlistManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNullableForUserRoomId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Rooms_RoomId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Rooms_RoomId",
+                table: "Users",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Rooms_RoomId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Rooms_RoomId",
+                table: "Users",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
