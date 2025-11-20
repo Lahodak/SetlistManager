bb8e81a9eb1477aa9a571a043070886c8cfc6203
Adam Lahuta <lahutad@student.uzlabina.cz>
2025-11-20 18:24:18 +0100

    Předělávání room UI

---
+++ SetlistManager.App/wwwroot/js/room.js
@@ -0,0 +1,9 @@
+﻿window.scrollToCurrentSong = function () {
+    const container = document.getElementById('setlist-container');
+    const currentSongCard = document.querySelector('.current-song-card');
+
+    if (container && currentSongCard) {
+        // Scroll the current song to the top of the container
+        container.scrollTop = currentSongCard.offsetTop - container.offsetTop;
+    }
+}
